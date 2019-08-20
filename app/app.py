6b788e2337e80c6beedc1f387fffb60819cb305f
svetkesh <svetkesh@hotmail.com>
2019-08-20 10:21:10 +0300

    Add worker, task, requirements

---
+++ app/app.py
@@ -0,0 +1,82 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, make_response, abort
+try:
+    from .calculator import calculate
+except ImportError:
+    from calculator import calculate
+import logging
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+
+
+app = Flask(__name__)
+
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
+@auth.get_password
+def get_password(username):
+    if username == 'poker':
+        return 'poker'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route('/')
+def index():
+    return "It is poker calculator!"
+
+
+@app.route('/data', methods=['POST'])
+def add():
+    app.logger.info("in post /data")
+    data = request.get_json()
+    app.logger.info(f"data:{data}")
+    app.logger.info(f"data:{jsonify(data)}")
+    app.logger.info(f"data:{request}")
+    # return jsonify({'sum': data['x'] + data['y']})
+    return str(data)
+
+
+# simple test auth
+@app.route('/api/v1.0/calculate/<hand>', methods=['GET'])
+@auth.login_required
+def calculate_hand(hand):
+    app.logger.info(type(hand), hand)
+    # return jsonify({'tasks': tasks})
+    return jsonify(calculate(hand))
+
+
+@app.route('/api/v1.0/hands', methods=['POST'])
+def calculate_hands():
+
+    app.logger.info(request.get_json())
+
+    if not request.json: # or not 'player' in request.json:
+        abort(400)
+    # hand = {
+    #     'player': request.json['player'],
+    #     'hand': request.json['hand'],
+    #     # 'hand': request.json.get('hand', ""),
+    #     # 'done': False
+    # }
+
+    hand = request.json['hand']
+    app.logger.info(hand)
+
+    # return jsonify({'hand': hand}), 201
+    # return jsonify({'hand': calculate(hand)}), 201
+    return jsonify({'hand': calculate(hand)}), 201
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
---
+++ app/calculator.py
@@ -0,0 +1,98 @@
+import json
+from collections import Counter
+
+
+VALUES_ORDER = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"]
+
+
+def max_value(cards, reverse=False):
+    try:
+        indexes = [VALUES_ORDER.index(c) for c in cards]
+        # sorted(indexes, reverse=reverse)
+        list.sort(indexes, reverse=reverse)
+        print(indexes)
+        return VALUES_ORDER[indexes[0]]
+    except Exception:
+        # hand = hand.split(",")
+        raise Exception(f"ERROR not a valid format for cards: {cards}")
+
+
+def calculate(hand):
+
+    cnt = Counter()
+    print(hand)
+
+    try:
+        # report = {}
+        result = 0
+        rank = 0
+
+        cards = [(c[0], c[1:]) for c in hand]
+        print(f"cards: {cards}")
+        card_values = [c[1] for c in cards]
+        print(f"card_values: {card_values}")
+        card_m = [c[0] for c in cards]
+        print(f"card_m: {card_m}")
+
+        if not isinstance(hand, list):
+            # hand = hand.split(",")
+            raise Exception(f"ERROR not a valid format, expected list, given: {type(hand)}")
+        if len(hand) != 5:
+            raise Exception(f"ERROR not a valid format, expected 5 cards, given: {len(hand)}")
+
+        # max card
+        max_card = max_value(card_values, reverse=True)
+        print(f"max_card: {max_card}")
+
+        min_card = max_value(card_values)
+        print(f"min_card: {min_card}")
+
+        # using Counter
+        two_most_often_values = Counter(card_values).most_common(2)
+        print(two_most_often_values[0])
+        print(two_most_often_values[1])
+
+        two_most_often_m = Counter(card_m).most_common(2)
+        print(two_most_often_m[0])
+
+        most_m = two_most_often_m[0][1]
+
+        most_0 = two_most_often_values[0][1]
+        most_1 = two_most_often_values[1][1]
+
+        if most_0 == 2:
+            rank = 1    # pair
+        if most_0 == 3:
+            rank = 2    # set
+        if most_0 == 2 and most_1 == 2:
+            rank = 3    # two pairs
+
+        if most_m == 5:
+            rank = 4    # flash
+
+        if VALUES_ORDER.index(max_card) - VALUES_ORDER.index(min_card) == 5:
+            rank = 5    # street
+
+        if most_0 == 3 and most_1 == 2:
+            rank = 6    # house
+
+        # ...
+
+        print(f"calculate found:{rank}")
+
+        return {"rank": rank, "max_card": max_card}
+
+    except Exception as e:
+        return {"result": str(e)}
+
+
+def process_hands(hands):
+    """Espect dictionary of players with hands"""
+    try:
+        report = {}
+        for player in hands:
+            report[player] = calculate(hands[player])
+        return report
+    except Exception as e:
+        raise Exception(f"ERROR while processing hands: {type(e), e}")
+
