8e9cfd1c078a7dc6f1da5325660090ab332e27d4
svetkesh <svetkesh@hotmail.com>
2019-08-20 15:27:16 +0300

    Add description

--- app/calculator.py
+++ app/calculator.py
@@ -18,7 +18,32 @@ def max_value(cards, reverse=False):
 
 
 def calculate(hand):
-    """Calculate poker cards wining combination rank, higher - better and higher card"""
+    """Calculate poker cards wining combination rank, higher - better and higher card
+
+    Accepts cards in format
+    'XY' , where X is suit
+         'Y' stands for card value, could be any in
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "K", "A"
+
+    For given request:
+
+    POST 127.0.0.1:5005/api/v1.0/hands
+    {
+        "hand": ["DA", "CA", "D7", "D6", "D3"]
+    }
+
+    returns:
+    {
+      "hand": {
+        "max_card": "A",
+        "rank": 1
+      }
+    }
+
+    where rank 1 stands for 'pair of'
+    and A for ACE
+
+    """
     # print(hand)
     try:
         # report = {}
