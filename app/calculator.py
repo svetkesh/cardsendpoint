70a55c23889f2e42d3558103abc10dc001b6a196
svetkesh <svetkesh@hotmail.com>
2019-08-20 16:49:12 +0300

    Add bug fixed with detecting high card in combinations

--- app/calculator.py
+++ app/calculator.py
@@ -11,7 +11,7 @@ def max_value(cards, reverse=False):
         # sorted(indexes, reverse=reverse)
         list.sort(indexes, reverse=reverse)
         print(indexes)
-        return VALUES_ORDER[indexes[0]]
+        return VALUES_ORDER[indexes[-1]]
     except Exception:
         # hand = hand.split(",")
         raise Exception(f"ERROR not a valid format for cards: {cards}")
@@ -50,24 +50,29 @@ def calculate(hand):
         result = 0
         rank = 0
 
-        cards = [(c[0], c[1:]) for c in hand]
-        print(f"cards: {cards}")
-        card_values = [c[1] for c in cards]
-        print(f"card_values: {card_values}")
-        card_m = [c[0] for c in cards]
-        print(f"card_m: {card_m}")
+        if isinstance(hand, str):
+            print(f"DBG processing string hand: {type(hand), hand}")
+            hand = hand.split(",")
 
         if not isinstance(hand, list):
             # hand = hand.split(",")
             raise Exception(f"ERROR not a valid format, expected list, given: {type(hand)}")
+
         if len(hand) != 5:
             raise Exception(f"ERROR not a valid format, expected 5 cards, given: {len(hand)}")
 
+        cards = [(c[0], c[1:]) for c in hand]
+        # print(f"cards: {cards}")
+        card_values = [c[1] for c in cards]
+        # print(f"card_values: {card_values}")
+        card_m = [c[0] for c in cards]
+        # print(f"card_m: {card_m}")
+
         # max card
-        max_card = max_value(card_values, reverse=True)
+        max_card = max_value(card_values)
         print(f"max_card: {max_card}")
 
-        min_card = max_value(card_values)
+        min_card = max_value(card_values, reverse=True)
         print(f"min_card: {min_card}")
 
         # using Counter
@@ -82,6 +87,9 @@ def calculate(hand):
 
         most_0 = two_most_often_values[0][1]
         most_1 = two_most_often_values[1][1]
+
+        if most_0 > 1:
+            max_card = two_most_often_values[0][0]
 
         if most_0 == 2:
             rank = 1    # pair
